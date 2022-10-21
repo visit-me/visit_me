c76ca627b6567ce08ca16b74582afe00af544e10
Tulio-Rangel <tjrn20@hotmail.com>
2022-10-21 18:53:26 -0500

    add language dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:visit_me/pages/register_page.dart';
 
 void main() {
@@ -12,6 +13,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Visit-Me',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('es', 'CO'),
+      ],
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
