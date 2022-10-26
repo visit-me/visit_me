47c65d400c75dd2df7123794ee8512d4e5e5c9fa
Tulio-Rangel <tjrn20@hotmail.com>
2022-10-25 19:22:31 -0500

    login function in firebase api file

--- lib/repository/firebase_api.dart
+++ lib/repository/firebase_api.dart
@@ -13,4 +13,17 @@ class FirebaseApi {
       return e.code;
     }
   }
+
+  Future<String?> logInUser(String email, String password) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+      return credential.user?.uid;
+    } on FirebaseAuthException catch (e) {
+      print('FirebaseAuthException ${e.code}');
+      return e.code;
+    } on FirebaseException catch (e) {
+      print('FirebaseException ${e.code}');
+      return e.code;
+    }
+  }
 }
